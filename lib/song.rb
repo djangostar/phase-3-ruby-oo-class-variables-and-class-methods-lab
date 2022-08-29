0caf7c937ce5f82b262a785de4ff1fbac2a728ec
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-08-28 21:04:37 -0700

    commit tree 3a95bb070c0843cd61b0222f26a369975172cce0

--- lib/song.rb
+++ lib/song.rb
@@ -4,13 +4,14 @@ class Song
     @@count = 0
     @@artists = []
     @@genres = []
+
     def initialize(name, artist, genre)
         @name = name
         @artist = artist
         @genre = genre
         @@count += 1
-        @@artists << self.artist
-        @@genres << self.genre
+        @@artists << artist
+        @@genres << genre
     end
 
     def self.count
@@ -25,13 +26,10 @@ class Song
         @@genres.uniq
     end
 
-    # def self.genre_count
-    #     @@genres.tally
-    # end
-
     def self.genre_count
+        # @@genres.tally
         genre_count = {}
-        @@genres.each do | genre |
+        @@genres.each do |genre|
             if genre_count.key?(genre)
                 genre_count[genre] += 1
             else
